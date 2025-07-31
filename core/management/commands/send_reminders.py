9ce1b4ee0fdcc24e9ae65af142ac301eb52c1e46
Enyelbert <epantah@ulasalle.edu.pe>
2025-07-31 13:22:48 -0500

    cambiando structur

--- core/management/commands/send_reminders.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from core.tasks import verificar_y_enviar_mensajes
-
-class Command(BaseCommand):
-    help = 'Envía recordatorios por WhatsApp según la fecha de vencimiento'
-
-    def handle(self, *args, **kwargs):
-        verificar_y_enviar_mensajes()
